eb4282322b6825b31ccd66fddfb94b7b89d9a9bd
Michael Durrant <m2@snap2web.com>
2020-03-23 14:59:36 -0400

    javascript for HTML

--- javascript_for_HTML_form.js
+++ javascript_for_HTML_form.js
@@ -19,8 +19,30 @@ const po = pageObjects;
   po.billingStreets='table#billing tr input[id^=billing-street-';
   po.billingState='select#billing-state';
   po.billingZip='input#billing-zip';
-  po.StreetText = (num) => `Street${num}`;
-}
+  po.StreetText = num => `Street${num}`;
+  po.HomeStreetText = row => $(`${po.home} input#home-street-${row}`).val();
+  po.newStreet = nextStreetNum => `input#home-street-${String(nextStreetNum)}`;
+  po.billingStreetHTML = row => `table#billing input#billing-street-${row}`;
+  po.addressLineHTML = (newRowText, addressType, streetRowNum) => {
+    const boldIt = streetRowNum > 2;
+    var [boldStart, boldEnd] = bolder(boldIt);
+    return `${boldStart}${newRowText}${boldEnd}&nbsp;<input type=text id=${addressType}-street-${String(streetRowNum)}>`;
+  };
+  po.billingNewRowHTML = (billingNewRowText, billingNewRowNum, boldIt = false) => {
+    var [boldStart, boldEnd] = bolder(boldIt);
+    return `${boldStart}${billingNewRowText}${boldEnd}&nbsp;<input type=text id=billing-street-${String(billingNewRowNum)}>`;
+  };
+  po.billingHTML = (streetNumLabel, streetNum) => {
+    return `<b>Street${streetNumLabel}</b><input id=billing-street-${streetNum}>`;
+  };
+};
+
+const bolder = (boldIt = false) => {
+   let boldStart, boldEnd;
+   [boldStart, boldEnd] = boldIt ? ['<b>', '</b>'] : ['', ''];
+   return ([boldStart, boldEnd]);
+};
+
 $(document).ready(function(){
   $("select#home-state").change(function(){
     homeBillingLinked() && copyHomeStateToBilling(this);
@@ -28,12 +50,11 @@ $(document).ready(function(){
   $("input#home-zip").change(function(){
     homeBillingLinked() && copyHomeZipToBilling(this);
   });
-  $("input#home-street-1").change(function(){
-    homeBillingLinked() && copyHomeStreetnToBilling(this,1);
-  });
-  $("input#home-street-2").change(function(){
-    homeBillingLinked() && copyHomeStreetnToBilling(this,2);
-  });
+  for(let streetNumber=1; streetNumber <= 2; streetNumber++) {
+    $(`input#home-street-${streetNumber}`).change(function () {
+      homeBillingLinked() && copyHomeStreetnToBilling(this, streetNumber);
+    })
+  }
 });
 const addStreetAddressLine = () => { // eslint-disable-line no-unused-vars
   const streetRowCount = $(po.homeStreets).length;
@@ -42,9 +63,8 @@ const addStreetAddressLine = () => { // eslint-disable-line no-unused-vars
   if(streetRowCount >= maxStreetLines) p('max streets reached');
   else {
     createNewAddressLine(po.home, nextStreetNum);
-    console.log(nextStreetNum);
     homeBillingLinked() && createNewAddressLine(po.billing, nextStreetNum);
-    $(`input#home-street-${String(nextStreetNum)}`).change(function() {
+    $(po.newStreet(nextStreetNum)).change(function() {
       homeBillingLinked() && copyHomeStreetnToBilling(this,nextStreetNum);
     });
   }
@@ -54,17 +74,17 @@ const createNewAddressLine = (table, streetRowNum) => {
   const addressTableHTML = $(table)[0];
   const skipHTMLRows = 3;
   const billingOffset = table === po.billing ? 1 : 0;
+  const addressType = table === po.billing ? 'billing' : 'home'
   const zeroIndexOffset = 1;
   const newRowNumber = skipHTMLRows + (streetRowNum - zeroIndexOffset) - billingOffset;
   const newHTMLRow = addressTableHTML.insertRow(newRowNumber);
-  newHTMLRow.insertCell(0).innerHTML =
-    `<b>${newRowText}</b>&nbsp;<input type=text id=home-street-${String(streetRowNum)}>`;
+  newHTMLRow.insertCell(0).innerHTML = po.addressLineHTML(newRowText, addressType, streetRowNum);
 };
 const removeStreetAddressLine = () => {  // eslint-disable-line no-unused-vars
   const homeRows = $(po.homeRows);
   const streetRowCount = $(po.homeStreets).length;
-  const skipRows = 2;
-  const homeRowToRemove = skipRows + streetRowCount;
+  const skipTableRows = 2;
+  const homeRowToRemove = skipTableRows + streetRowCount;
   const billingOffset = 1;
   const billingRowToRemove = homeRowToRemove - billingOffset
   const billingRows = $(po.billingRows);
@@ -81,6 +101,7 @@ const copyHomeToBilling = () => {  // eslint-disable-line no-unused-vars
     copyHomeStateToBilling(po.homeState);
     copyHomeZipToBilling();
     [homeRowsCount, billingRowsCount] = countStreets();
+    p(`homeRowsCount = ${homeRowsCount}, billingRowsCount = ${billingRowsCount}`)
     const streetDiffCount = homeRowsCount - billingRowsCount;
     if (streetDiffCount > 0) addNewHomeAddressLinesToBilling(streetDiffCount);
     else if(streetDiffCount < 0) resetBillingAddressLines();
@@ -95,8 +116,9 @@ const addNewHomeAddressLinesToBilling = (numStreets) => {
     const billingNewRowText = po.StreetText(extraHomeStreet);
     const billingTable = $(po.billing)[0];
     const billingNewRow = billingTable.insertRow(firstLineNum + billingNewRowNum - 2);
-    const newHTML = `${billingNewRowText}&nbsp;<input type=text id=billing-street-${String(billingNewRowNum)}>`;
-    billingNewRow.insertCell(0).innerHTML = newHTML;
+    const boldIt = billingNewRowNum > 2 ? true : false
+    billingNewRow.insertCell(0).innerHTML =
+    po.billingNewRowHTML(billingNewRowText, billingNewRowNum, boldIt);
   }
 };
 const resetBillingAddressLines = () => {
@@ -104,24 +126,26 @@ const resetBillingAddressLines = () => {
   deleteExistingAddressLines(billingTable);
   createNewAddressLines(billingTable);
 };
-const createNewAddressLines = (billingTable) => {
-  for (let i = 1; i <= 2; i++) {
-    const billingNewRow = billingTable.insertRow(i + 1);
-    billingNewRow.insertCell(0).innerHTML = `<b>Street${i}</b>&nbsp;<input type=text id=billing-street-${i}>`;
-  }
-};
 const deleteExistingAddressLines = (billingTable) => {
   const numLines = $(po.billingStreets).length;
   for (let i = 1; i <= numLines; i++) {
-    const insertRow = 2;
-    billingTable.deleteRow(insertRow);
+    const topRow = 2;
+    billingTable.deleteRow(topRow);
+  }
+};
+const createNewAddressLines = (billingTable) => {
+  for (let streetNum = 1; streetNum <= 2; streetNum++) {
+    const billingNewRow = billingTable.insertRow(streetNum + 1);
+    const streetNumText = streetNum == 1 ? '' : streetNum
+    const streetNumLabel = `Street${streetNumText}`;
+    billingNewRow.insertCell(0).innerHTML = po.billingNewRowHTML(streetNumLabel, streetNum);
   }
 };
 const copyAddressLineValues = () => {
   const numLines = $(po.homeStreets).length;
   for (let row = 1; row <= numLines; row++) {
-    const homeStreetValue = $(`table#home input#home-street-${row}`).val();
-    const billingStreet = $(`table#billing input#billing-street-${row}`)[0];
+    const homeStreetValue = po.HomeStreetText(row);
+    const billingStreet = $(po.billingStreetHTML(row))[0];
     billingStreet.value = homeStreetValue;
   }
 };
